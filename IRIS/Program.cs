77e6b84f1a33b41d94a9075bfccbb04c6aa42840
KostelBeats <67270862+KostelBeats@users.noreply.github.com>
2023-11-14 01:29:47 +0300

    Removed WPF

---
+++ IRIS/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.IO.Ports;
+using IRIS.DataFlow.Connection;
+namespace IRIS;
+
+public class Program
+{
+    public static void Init()
+    {
+        System.Diagnostics.Debug.WriteLine("Started init.");
+        DataFlowSerial connection = new(new SerialPort("COM 11", 115200));
+        connection.Connect();
+        //connection.ColorWheel(64, 0.01);
+        //connection.Strobe(new DataFlow.Color.DataFlowRGB(255, 0, 0), 512);
+        //connection.SetColor(new DataFlow.Color.DataFlowRGB(255, 0, 255));
+        connection.HarshRandom(64, 0.1);
+            
+    }
+
+    public static void Main()
+    {
+        System.Diagnostics.Debug.WriteLine("hi");
+        Init();
+    }
+}
